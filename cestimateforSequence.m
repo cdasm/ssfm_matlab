d462c441ed2c822601c5e9c4d2b78c986e473c1a
unknown <wangzp@cvl.iis.u-tokyo.ac.jp>
2014-09-04 16:22:39 +0900

    p

---
+++ cestimateforSequence.m
@@ -0,0 +1,54 @@
+function [transitions,rotations,pts]=cestimateforSequence(seq,goldpts,pntpos)
+
+fnms=readLst(seq);
+
+gptcorres=areadLst(goldpts);
+pntposs=areadLst(pntpos);
+
+nframes=length(fnms);
+npts=length(gptcorres);
+
+rotations=zeros(nframes,3);
+transitions=zeros(nframes,3);
+pts=zeros(npts,3);
+goodmark=zeros(npts,1);
+
+
+skptss=cell(nframes,1);
+indss=cell(nframes,1);
+iptss=cell(npts,1);
+
+for i=1:nframes
+    tmp=load(fnms{i});
+    ind=tmp(:,1);    kpt=tmp(:,2:3);      n=mylength(kpt);
+    skptss{i}=zeros(n,3);
+    for j=1:n
+        skptss{i}(j,:)=im2Serph(kpt(j,:),[512,256]);
+    end
+    indss{i}=ind;
+end
+
+for i=1:npts
+    n=mylength(pntposs{i});
+    iptss{i}=zeros(n,3);
+    
+    for j=1:n
+        frmind=gptcorres{i}(j);
+        iptss{i}(j,:)=skptss{frmind}(pntposs{i}(j),:);
+    end
+end
+
+
+for i=2:nframes
+ 
+    skpt1=skptss{i-1};
+    ind1=indss{i-1};
+    skpt1=(rotateMM(rotations(i-1,:))*skpt1')';
+    
+    skpt2=skptss{i};
+    ind2=indss{i};
+    matches=matchBetweenTwoV(ind1,ind2);
+end
+
+
+end
