9f77edb9203224be343e2a93480e1f64f219ed10
unknown <wangzp@cvl.iis.u-tokyo.ac.jp>
2014-09-23 22:57:27 +0900

    p

---
+++ estimateforSequence_g.m
@@ -0,0 +1,57 @@
+function [transitions,rotations,pts]=estimateforSequence_g()
+
+seq='gpts.lst';
+goldpts='goldenpnt.lst';
+pntpos= 'goldencorres.lst';
+[fnms,gptcorres,pntposs,nframes,npts,skptss,indss,iptss]=prepareForSequence(seq,goldpts,pntpos);
+
+rotations=cell(nframes);
+rotations{1}=eye(3);
+transitions=zeros(nframes,3);
+pts=zeros(npts,3);
+goodmark=zeros(npts,1);
+
+for i=2:nframes
+    i
+    skpt1=skptss{i-1};
+    ind1=indss{i-1};
+    skpt1=(rotations{i-1}*skpt1')';
+    
+    skpt2=skptss{i};
+    ind2=indss{i};
+    matches=matchBetweenTwoV(ind1,ind2);
+    
+    [tran,rot,gscore]=TARfromTPntSet_c(skpt1(matches(:,1),:),skpt2(matches(:,2),:));
+    gscore
+    [~,idx]=sort(gscore);
+    
+    cad1=idx(mylength(idx));
+    cad2=idx(mylength(idx)-1); 
+  
+ 
+    tran1=tran(cad1,:);
+    rot1=rot{cad1};
+    
+    sc=1;
+    if i>2
+        skpt1=skptss{i-2};
+        ind1=indss{i-2};
+        skpt1=(rotations{i-2}*skpt1')';
+        
+         matches=matchBetweenTwoV(ind1,ind2);
+    
+        [tran,~,gscore]=TARfromTPntSet_c(skpt1(matches(:,1),:),skpt2(matches(:,2),:));
+        %gscore
+        [~,idx]=sort(gscore);
+
+        cad1=idx(mylength(idx));
+        
+        tran2=tran(cad1,:);
+        %sc=bestScale_c(transitions(i-2,:),tran2,transitions(i-1,:),tran1)
+    end
+    
+    transitions(i,:)=transitions(i-1,:)+tran1*sc;
+    rotations{i}=rot1;
+ 
+    waitforbuttonpress
+end
