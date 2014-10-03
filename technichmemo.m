96e15d60e35b419eb48aa7cf4d1f4c5f3236c17a
unknown <wangzp@cvl.iis.u-tokyo.ac.jp>
2014-10-03 12:52:07 +0900

    ok

--- technichmemo.m
+++ technichmemo.m
@@ -28,3 +28,14 @@ waitforbuttonpress;
 matlabpool('open',4);
 parfor i=1:3, c(:,i) = eig(rand(1000)); end
 matlabpool('close');
+
+%this is a method to generate animation
+figure
+h=plot3(a(:,1),a(:,2),a(:,3));
+pause(0.5);
+axis equal
+for ii=1:num
+set(h(1),'xdata',a(1:ii,1),'ydata',a(1:ii,2),'zdata',a(1:ii,3)) ;
+pause(0.1);drawnow;
+saveas(h,sprintf('ww%d',ii),'jpg')%this line saves the image
+end
