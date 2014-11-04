46dbf762c2c1d43d6aa73fd08cb17f729eeb2867
unknown <wangzhp85@gmail.com>
2014-11-04 15:14:37 +0900

    p

--- outputcpp.m
+++ outputcpp.m
@@ -1,7 +1,13 @@
-function outputcpp(inp)
+function outputcpp(inp,myc)
 
 ccode(inp,'file','tem.txt')
 
-system('python convertMatalb2Eigen.py tem.txt atem.txt')
+if nargin<2
+    system('python convertMatalb2Eigen.py tem.txt atem.txt')
+else
+    cm=sprintf('python convertMatalb2Eigen.py tem.txt atem.txt %s',myc)
+
+    system(cm)
+end
 
 edit 'atem.txt'
